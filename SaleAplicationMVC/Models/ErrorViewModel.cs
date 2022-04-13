63984b3958a35ba0795f90851f5eaea17bc09879
Your Name <you@example.com>
2022-04-12 23:13:24 -0300

    Initializing

---
+++ SaleAplicationMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SaleAplicationMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
