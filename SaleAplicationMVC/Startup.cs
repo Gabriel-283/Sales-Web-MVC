284e5f86e5488c4ad98887962da7c95a9c7cf097
Your Name <you@example.com>
2022-04-18 21:59:41 -0300

    Initializing DataBase Connection

--- SaleAplicationMVC/Startup.cs
+++ SaleAplicationMVC/Startup.cs
@@ -37,7 +37,8 @@ namespace SaleAplicationMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SaleAplicationMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SaleAplicationMVCContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SaleAplicationMVCContext"), builder =>
+                        builder.MigrationsAssembly("SaleAplicationMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
