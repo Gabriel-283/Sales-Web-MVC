9c9e2edb9683425466d68ae9e4f648633c090bfb
Your Name <you@example.com>
2022-05-01 19:26:04 -0300

    Insert Seller page

--- SaleAplicationMVC/Controllers/SellersController.cs
+++ SaleAplicationMVC/Controllers/SellersController.cs
@@ -3,14 +3,38 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SaleAplicationMVC.Services;
+using SaleAplicationMVC.Models;
 
 namespace SaleAplicationMVC.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
         public IActionResult Index()
         {
+            _sellerService.ShowAll();
+            var list = _sellerService.ShowAll();
+            return View(list);
+        }
+
+        public IActionResult Add()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Add(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ SaleAplicationMVC/Services/SellerService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SaleAplicationMVC.Data;
+using SaleAplicationMVC.Models;
+
+namespace SaleAplicationMVC.Services
+{
+    public class SellerService
+    {
+
+        private readonly SaleAplicationMVCContext _context;
+
+        public SellerService(SaleAplicationMVCContext context)
+        {
+            this._context = context;
+        }
+
+        public List<Seller> ShowAll()
+        {
+            return this._context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            //obj.Department = ;
+            _context.Add(obj);
+            _context.SaveChanges();
+
+        }
+    }
+}
